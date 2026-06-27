82701d80b6b3317ece33024f1ba79926e3168824
SaviProgrammer10 <savita.dhingra@codingal.com>
2026-06-27 13:20:09 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,45 @@
+// A sample class Shape which provides a method to get the Shape's area
+class Shape {
+    public double getArea() {
+    return 0;
+    }
+}
+
+// A triangle with base and height
+class Triangle extends Shape { // extended form the Shape class
+    private double base;
+    private double height;
+    public Triangle(double base, double height) {
+    this.base = base;
+    this.height = height;
+    }
+
+    public double getArea() { //area of triangle 1/2*b*h
+    //area using heron's formula can also be used here
+    return 0.5 * base * height;
+    }
+}
+
+// A SQUARE is a Shape with a specific side
+class Square extends Shape {
+    private double side;
+    public Square(double side) {
+    this.side = side;
+    }
+
+    public double getArea() {
+    return side * side;
+    }
+}
+
+class Area {
+    public static void main(String args[]) {
+    Shape[] shape = new Shape[2]; // Creating shape array of size 2
+    
+    shape[0] = new Square(5); // creating circle object at index 0
+    shape[1] = new Triangle(5, 8); // creating rectangle object at index 1
+   
+    System.out.println("Area of the Square: " + shape[0].getArea());
+    System.out.println("Area of the Triangle: " + shape[1].getArea());
+    }
+}
